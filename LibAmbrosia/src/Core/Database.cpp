73cfc3c205295672415341b55b36e20162a0e68b
Terodom <terodom@terodom.com>
2021-02-04 22:12:32 +0100

    Initial Commit

---
+++ LibAmbrosia/src/Core/Database.cpp
@@ -0,0 +1,17 @@
+#include "Core/Database.hpp"
+
+namespace Ambrosia
+{
+    namespace Core
+    {
+        Database::Database() {
+
+        }
+
+        Database::~Database() {
+
+        }
+
+        void Database::Init();
+    }
+}
